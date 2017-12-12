7453092e5b02d591f1f77df8fd57aaec50516202
Mario Lang <mlang@delysid.org>
2017-12-12 18:58:12 +0100

    package/emacs: cleanup

--- package/emacs/emacs.mk
+++ package/emacs/emacs.mk
@@ -1,8 +1,8 @@
-# cheap hack (so far):
-# mkdir /etc/qemu-binfmt
-# ln -s $(TARGET_DIR) /etc/qemu-binfmt/arm
+################################################################################
 #
-# After, that, emacs cross-builds
+# emacs
+#
+################################################################################
 
 EMACS_VERSION = master
 EMACS_SITE = https://git.savannah.gnu.org/git/emacs.git
@@ -15,9 +15,13 @@ EMACS_AUTORECONF_OPTS = -fi -Im4
 #EMACS_SOURCE = emacs-$(EMACS_VERSION).tar.xz
 #EMACS_SITE = $(BR2_GNU_MIRROR)/emacs
 
-EMACS_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES) dbus gnutls host-qemu jansson libxml2 zlib
+EMACS_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES) dbus gnutls jansson libxml2 zlib
 
+ifeq ($(BR2_INIT_SYSTEMD),y)
+EMACS_DEPENDENCIES += systemd
+endif
+
+EMACS_DEPENDENCIES += host-qemu
 EMACS_CONF_OPTS += --with-emulator="$(QEMU_USER) -L $(TARGET_DIR)"
-EMACS_MAKE = $(MAKE1)
 
 $(eval $(autotools-package))
