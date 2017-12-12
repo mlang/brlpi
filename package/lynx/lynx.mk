715d4aee1b81f55bc5c06652affc5494ccbb6b69
Mario Lang <mlang@delysid.org>
2017-12-12 19:33:36 +0100

    package/lynx: Support for slang and wide curses

--- package/lynx/lynx.mk
+++ package/lynx/lynx.mk
@@ -10,8 +10,15 @@ LYNX_SITE = ftp://ftp.invisible-island.net/lynx/tarballs
 LYNX_LICENSE = GPL-2.0
 LYNX_LICENSE_FILES = COPYING
 
-LYNX_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES) ncurses
-LYNX_CONF_OPTS = --with-screen=ncurses --with-curses-dir=$(TARGET_DIR)/usr
+LYNX_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
+
+ifeq ($(BR2_PACKAGE_NCURSES),y)
+LYNX_DEPENDENCIES += ncurses
+LYNX_CONF_OPTS += --with-screen=ncurses$(if $(BR2_PACKAGE_NCURSES_WCHAR),w)
+else ifeq ($(BR2_PACKAGE_SLANG),y)
+LYNX_DEPENDENCIES += slang
+LYNX_CONF_OPTS += --with-screen=slang
+endif
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 LYNX_DEPENDENCIES += openssl
