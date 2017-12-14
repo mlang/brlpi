d2b94ed2fa08d55fd18e28995d20a2c3edf4da46
Mario Lang <mlang@delysid.org>
2017-12-14 23:45:48 +0100

    Enable emacs for user brlpi only if it has been installed

--- external.mk
+++ external.mk
@@ -1,5 +1,15 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_BRLPI_PATH)/package/*/*.mk))
 
+ifeq ($(BR2_PACKAGE_EMACS),y)
+define BRLPI_ENABLE_EMACS
+	mkdir -p $(TARGET_DIR)/home/brlpi/.config/systemd/user/default.target.wants
+	ln -sf ../../../../../../usr/lib/systemd/user/emacs.service \
+		$(TARGET_DIR)/home/brlpi/.config/systemd/user/default.target.wants/emacs.service
+endef
+
+EMACS_POST_INSTALL_TARGET_HOOKS += BRLPI_ENABLE_EMACS
+endif
+
 define BRLPI_CREATE_USER_OVERLAY
 	mkdir -p $(HOME)/.config/brlpi/overlay
 endef
